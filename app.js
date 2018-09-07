375ef9fdb9da814b999e00aedc4a66afa325e6fb
tiwarir1 <rabi.tiwari5788@gmail.com>
2018-09-07 17:11:34 -0500

    Feat: error handling, logging and nodemon

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express= require('express');
 const app = express();
+const morgan = require('morgan');
+
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((request, response, next) => {
+	const error = new Error('Not Found');
+	error.status = 404;
+	next(error);
+});
 
+app.use((error, request, response, next) => {
+	response.status(error.status || 500);
+	response.json({
+		error: {
+			message: error.message
+		}
+	})
+})
 
 module.exports= app;
